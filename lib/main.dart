a141855d1d628807cde926732c8ad11f82fbcd61
Bk-09 <amicodes23@gmail.com>
2024-06-10 18:39:47 +0530

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noteits/firebase_options.dart';
+import 'package:noteits/views/login_view.dart';
+import 'package:noteits/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,6 +12,11 @@ void main() {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/':(context) => const LoginView(),
+        '/register/':(context) => const RegisterView(),
+
+      },
     ));
 }
 
@@ -23,6 +29,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Home Page")
         ),
+
       body:  FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
@@ -31,14 +38,14 @@ class HomePage extends StatelessWidget {
 
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-            final user=FirebaseAuth.instance.currentUser;
-            if(user?.emailVerified ?? false){
-              print("You are a verified user");
-            }else{
-              print("Please verify your email.");
-            }
-             return const Text('Done');
+            // final user=FirebaseAuth.instance.currentUser;
+            // if(user?.emailVerified ?? false){
+            //   return const Text('Done');
+            // }else{
+              // return const VerifyEmailView();
+            // }
 
+            return const LoginView();
         default: 
         return const Text('Loading...');
           }
@@ -48,3 +55,5 @@ class HomePage extends StatelessWidget {
     );
   }
   }
+
+
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:noteits/firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
@@ -30,63 +28,52 @@ class _RegisterViewState extends State<RegisterView> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Register')),
-      body: FutureBuilder(
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-             return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(hintText: "Enter your email here"),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: InputDecoration(hintText: 'Enter your password'),
-            ),
-            TextButton(onPressed: () async{
-              
-              final email=_email.text;
-              final password=_password.text;
-
-              try{
-                final UserCredential=
-               FirebaseAuth.instance.createUserWithEmailAndPassword(
-                email: email, 
-                password: password
-                );
-                print(UserCredential);
-              }on FirebaseAuthException catch(e){
-                if(e.code=='weak-password'){
-                  print('Weak password');
-                }else if(e.code=='email-already-in-use'){
-                  print("User already exists");
-                }else if(e.code=='invalid-email'){
-                  print("Invalid Email entered");
-                }
-              }            
-            },
-            child:const Text('Register') ,),
-          ],
-        );
-
-        default: 
-        return const Text('Loading...');
-          }         
-        },
-      ),
-    );
+     return Scaffold(
+      appBar: AppBar(title: const Text('Register View'),),
+      body: Column(
+            children: [
+              TextField(
+                controller: _email,
+                enableSuggestions: false,
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(hintText: "Enter your email here"),
+              ),
+              TextField(
+                controller: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(hintText: 'Enter your password'),
+              ),
+              TextButton(onPressed: () async{
+                
+                final email=_email.text;
+                final password=_password.text;
+     
+                try{
+                  final UserCredential=
+                 FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: email, 
+                  password: password
+                  );
+                  print(UserCredential);
+                }on FirebaseAuthException catch(e){
+                  if(e.code=='weak-password'){
+                    print('Weak password');
+                  }else if(e.code=='email-already-in-use'){
+                    print("User already exists");
+                  }else if(e.code=='invalid-email'){
+                    print("Invalid Email entered");
+                  }
+                }            
+              },
+              child:const Text('Register') ,),
+              TextButton(onPressed: (){
+                Navigator.of(context).pushNamedAndRemoveUntil( '/login/', (route) => false);
+              }, child: const Text("Already registered? Login here"))
+            ],
+          ),
+     );
   }
 }
